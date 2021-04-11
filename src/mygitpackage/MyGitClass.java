a07ee75e1bffb6cecc3bb2a25901fa6e1aeffac3
isaaktas25 <avdarmali373@gmail.com>
2021-04-11 12:51:17 +0200

    git guncelmi

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -3,8 +3,8 @@ package mygitpackage;
 public class MyGitClass {
 
 	public static void main(String[] args) {
-		System.out.println("version11S");
-		
+		System.out.println("version11S49");
+		System.out.println("version1149");
 	}
 
 }
