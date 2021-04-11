f55d9d329d2909b8696459ccb99af865b5e02584
isaaktas25 <81901274+isaaktas25@users.noreply.github.com>
2021-04-11 13:32:15 +0200

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,6 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("version11S49");
 		System.out.println("version1149");
+		System.out.println("version1 github")
 	}
-
 }
